4582d01418c6c650bc2b7cd0307d557d2f25d2cc
QuangDai97 <ngoquangdai100397@gmail.com>
2024-05-29 09:59:06 +0700

    Add project files.

---
+++ AdminCreateUserDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminCreateUserDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
